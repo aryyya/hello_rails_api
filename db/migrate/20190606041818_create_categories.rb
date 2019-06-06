4437232bfe1b4febe0b6fcb51fb35e22420bfd90
aryyya <aryyya.xyz@gmail.com>
2019-06-05 21:28:54 -0700

    Add Category model.

---
+++ db/migrate/20190606041818_create_categories.rb
@@ -0,0 +1,15 @@
+class CreateCategories < ActiveRecord::Migration[5.2]
+  def change
+    create_table :categories do |t|
+      t.integer :id
+      t.string :title
+      t.integer :pages
+      t.integer :subcategories
+      t.integer :files
+      t.date :created_at
+      t.date :updated_at
+      t.timestamps
+    end
+    execute "ALTER TABLE categories ADD PRIMARY KEY (id);"
+  end
+end
